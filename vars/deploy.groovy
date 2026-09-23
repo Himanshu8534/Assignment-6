3f27351762ed25deed73f2c581ea2b0e776b74dc
Game_play👾🎮👾🎮 <hc8059118@gmail.com>
2026-09-24 02:46:29 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,3 +1,29 @@
 def call() {
-    echo "Shared Library Started"
+
+    echo "===== START PIPELINE ====="
+
+    // Step 1: Read config
+    def props = readProperties file: 'config.properties'
+
+    def repo = props['GIT_REPO']
+    def approval = props['KEEP_APPROVAL_STAGE']
+
+    // Step 2: Clone Ansible repo
+    sh "git clone ${repo}"
+
+    // Step 3: Approval
+    if (approval == "true") {
+        input message: "Approve deployment?"
+    }
+
+    // Step 4: Run Ansible
+    sh """
+        cd ansible-assignment-5
+        ansible-playbook playbook.yml -i inventory
+    """
+
+    // Step 5: Notification
+    echo "Deployment Completed"
+
+    echo "===== END PIPELINE ====="
 }
