ab5fe09c447b1a828c7ceb57439e19c9a568a920
Game_play👾🎮👾🎮 <hc8059118@gmail.com>
2026-09-24 02:51:11 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,29 +1,36 @@
 def call() {
 
-    echo "===== START PIPELINE ====="
+    node {
 
-    // Step 1: Read config
-    def props = readProperties file: 'config.properties'
+        echo "===== START PIPELINE ====="
 
-    def repo = props['GIT_REPO']
-    def approval = props['KEEP_APPROVAL_STAGE']
+        // Step 1: Read config file
+        def configText = readFile 'config.properties'
+        def config = configText.split('\n').collectEntries { line ->
+            def parts = line.split('=')
+            [(parts[0].trim()): parts[1].trim()]
+        }
 
-    // Step 2: Clone Ansible repo
-    sh "git clone ${repo}"
+        def repo = config['GIT_REPO']
+        def approval = config['KEEP_APPROVAL_STAGE']
 
-    // Step 3: Approval
-    if (approval == "true") {
-        input message: "Approve deployment?"
+        // Step 2: Clone repo
+        sh "git clone ${repo}"
+
+        // Step 3: Approval stage
+        if (approval == "true") {
+            input message: "Do you want to deploy?"
+        }
+
+        // Step 4: Run Ansible
+        sh """
+            cd ansible-assignment-5
+            ansible-playbook playbook.yml -i inventory
+        """
+
+        // Step 5: Notification
+        echo "Deployment Completed"
+
+        echo "===== END PIPELINE ====="
     }
-
-    // Step 4: Run Ansible
-    sh """
-        cd ansible-assignment-5
-        ansible-playbook playbook.yml -i inventory
-    """
-
-    // Step 5: Notification
-    echo "Deployment Completed"
-
-    echo "===== END PIPELINE ====="
 }
